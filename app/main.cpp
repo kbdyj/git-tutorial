0c1a3d80620e3914caf9bb84e804d3de9364a59a
kbdyj <kbdyj@kbdyjui-MacBook-Pro.local>
2016-08-25 10:27:48 +0900

    print hello-world

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,12 @@
 // main.cpp
 #include <QCoreApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    qDebug() << "hello world.";
 
     return a.exec();
 }
